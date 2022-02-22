a18544179f8b6529a1e6d2930c984f052c0f0b92
arbob <arbob@live.in>
2022-02-22 10:49:40 +0530

    conflict fix

--- webapp/app/common/utils/pipelineMappingUtils.tsx
+++ webapp/app/common/utils/pipelineMappingUtils.tsx
@@ -1,10 +1,10 @@
-import { ConnectorSchema, PipelineSchemaResponseDto } from "../dtos/PipelineSchemaResponseDto";
+import { PipelineSchemaResponseDto } from "../dtos/PipelineSchemaResponseDto";
 import { SelectOptionDto } from "../dtos/SelectOptionDto";
 
 const getAppSchemaFields = (
   connectorSchema: ConnectorSchema | undefined
 ): SelectOptionDto[] | undefined =>
-  connectorSchema?.fields.map((field) => ({
+  connectorSchema?.fields.map((field: { fieldName: any; }) => ({
     value: field.fieldName,
     title: field.fieldName,
   }));
