4e0c949992b10e3f5cd1fc158948abd478d6b679
Abhilash Thulaseedharan <abhilashtdharan@gmail.com>
2022-03-02 20:44:39 +0530

    migration script

--- container/src/main/resources/migration/V18__migration_details.sql
+++ container/src/main/resources/migration/V18__migration_details.sql
@@ -2,7 +2,7 @@ CREATE TABLE `migration_details` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `migration_type` varchar(50) NOT NULL,
   `migration_status` varchar(20) NOT NULL,
-  `details` text NULL,
+  `details` text NOT NULL,
   `created_ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
