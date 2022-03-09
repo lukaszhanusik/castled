a0a2565640e187990c81e96616c46a51d57b8816
arbob <arbob@live.in>
2022-03-09 13:13:40 +0530

    popup overlay background shading

--- webapp/app/components/layout/WelcomePopup.tsx
+++ webapp/app/components/layout/WelcomePopup.tsx
@@ -16,61 +16,61 @@ export default function WelcomePopup() {
       position="right center"
       modal
       closeOnDocumentClick
-      className="popup-content"
+      className="popup-content popup-overlay"
       onClose={closeModal}
     >
-        <div className="card-body popup-body py-5">
-          <div className="container">
-            <div className="row py-4 px-5">
-              <div className="col-6">
-                <img
-                  src="/images/Castled-Logo.png"
-                  alt="Castled Logo"
-                  className="my-3"
-                />
-                <h2 className="header mb-3">Welcome to Castled</h2>
-                <h3 className="description mb-3">
-                  Enter your email to get latest updates on Castled.
-                </h3>
-                <Formik
-                  initialValues={{
-                    email: "",
-                  }}
-                  onSubmit={(values) => {
-                    closeModal();
-                    console.log(values);
-                  }}
-                >
-                  {({ values, setFieldValue, setFieldTouched }) => (
-                    <Form>
-                      <InputField
-                        type="email"
-                        name="email"
-                        title="Email"
-                        placeholder="Your email goes here"
-                      />
-                      <ButtonSubmit className="form-control btn-lg py-2">
-                        Enter
-                      </ButtonSubmit>
-                      <Button
-                        className="form-control btn-lg my-2"
-                        variant="outline-primary"
-                        onClick={closeModal}
-                      >
-                        Skip
-                      </Button>
-                    </Form>
-                  )}
-                </Formik>
-              </div>
-              <div className="col-6">
-                <img
-                  src="https://doodleipsum.com/700/flat?i=b27c0a34a1444c205ec7c49a651d3046"
-                  alt="Home Alone by Gustavo Pedrosa"
-                  className="popup-image"
-                />
-              </div>
+      <div className="card-body popup-body py-5" data-popup="tooltip">
+        <div className="container">
+          <div className="row py-4 px-5">
+            <div className="col-6">
+              <img
+                src="/images/Castled-Logo.png"
+                alt="Castled Logo"
+                className="my-3"
+              />
+              <h2 className="header mb-3">Welcome to Castled</h2>
+              <h3 className="description mb-3">
+                Enter your email to get latest updates on Castled.
+              </h3>
+              <Formik
+                initialValues={{
+                  email: "",
+                }}
+                onSubmit={(values) => {
+                  closeModal();
+                  console.log(values);
+                }}
+              >
+                {({ values, setFieldValue, setFieldTouched }) => (
+                  <Form>
+                    <InputField
+                      type="email"
+                      name="email"
+                      title="Email"
+                      placeholder="Your email goes here"
+                    />
+                    <ButtonSubmit className="form-control btn-lg py-2">
+                      Enter
+                    </ButtonSubmit>
+                    <Button
+                      className="form-control btn-lg my-2"
+                      variant="outline-primary"
+                      onClick={closeModal}
+                    >
+                      Skip
+                    </Button>
+                  </Form>
+                )}
+              </Formik>
             </div>
+            <div className="col-6">
+              <img
+                src="https://doodleipsum.com/700/flat?i=b27c0a34a1444c205ec7c49a651d3046"
+                alt="Home Alone by Gustavo Pedrosa"
+                className="popup-image"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </Popup>
