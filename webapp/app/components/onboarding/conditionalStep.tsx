05b0bb6dc44d2c5be8643b4b0bffc4884016904a
arbob <arbob@live.in>
2022-03-17 16:16:13 +0530

    workaround for auto dropdown when empty db or filled db

--- webapp/app/components/onboarding/conditionalStep.tsx
+++ webapp/app/components/onboarding/conditionalStep.tsx
@@ -5,23 +5,16 @@ import { WelcomeOnboardingData } from "./Welcome";
 
 export default function ConditionalStep({
   steps,
+  type,
 }: {
   steps: WelcomeOnboardingData[];
+  type: string;
 }) {
-  const router = useRouter();
-  const [activeStep, setActiveStep] = useState<string | undefined>();
-  const [countDone, setCountDone] = useState(0);
+  const [activeStep, setActiveStep] = useState<number | undefined>();
 
   useEffect(() => {
     const step = findRemainingStep();
-    if (step) {
-      setActiveStep(step.toString());
-    }
-    const countIsDone = steps.reduce(
-      (acc, curr) => (curr.isDone ? acc + 1 : 0),
-      0
-    );
-    setCountDone(countIsDone);
+    setActiveStep(step);
   }, [steps]);
 
   function findRemainingStep() {
@@ -32,58 +25,76 @@ export default function ConditionalStep({
       }
       index++;
     }
+    return 0;
   }
 
-  /* countDone is used here as a workaround for accordion defaultActiveKey which only 
-   works during first render. Usually first render will give us undefined. So we will create
-   a countDone state. initially it will also give undefined. Now since countDone is undefined, 
-   accordion won't render and will only render when both countDone and activeStep are defined, 
-   which gives us the correct render and then auto-opens the desired dropdown. */
   return (
     <>
-      {countDone && (
-        <Accordion defaultActiveKey={activeStep}>
-          {steps.map((step, index) => {
-            const Icon = step.icon;
-            return (
-              <Accordion.Item eventKey={`${index}`} key={index}>
-                <Accordion.Header>
-                  <div className="mx-2">
-                    {step.isDone ? (
-                      <img
-                        src="/images/check-filled.svg"
-                        alt="Completed or not"
-                        className={`p-0 border-0 ${
-                          step.isDone
-                            ? "onboarding done"
-                            : "onboarding not-done"
-                        }`}
-                        style={{ width: "1.2rem" }}
-                      />
-                    ) : (
-                      <Icon size={25} stroke={1} className="navbar-icon" />
-                    )}
-                  </div>
-                  <div className="mx-2">{step.title}</div>
-                </Accordion.Header>
-                <Accordion.Body>
-                  <div className="my-2">{step.description}</div>
-                  {step.buttonText && (
-                    <div className="my-3">
-                      <button
-                        className="btn btn-primary"
-                        onClick={() => router.push(step.onClickURL)}
-                      >
-                        {"+ " + step.buttonText}
-                      </button>
-                    </div>
-                  )}
-                </Accordion.Body>
-              </Accordion.Item>
-            );
-          })}
+      {type === "demo" && activeStep && (
+        <Accordion defaultActiveKey={`${activeStep}`}>
+          <AccordionBody steps={steps} />
         </Accordion>
       )}
+      {/* Below component is for when nothing is configured. Need to explicitly 
+      check undefined Otherwise it will render 0 string.*/}
+      {type === "primary" && activeStep === 0 && activeStep !== undefined && (
+        <Accordion defaultActiveKey={`${activeStep}`}>
+          <AccordionBody steps={steps} />
+        </Accordion>
+      )}
+      {/* Below component is for when atleast one thing is configured. Need to explicitly 
+      check undefined and greater than one done, Otherwise it will render 0 string.*/}
+      {type === "primary" && activeStep !== undefined && activeStep > 0 && (
+        <Accordion defaultActiveKey={`${activeStep}`}>
+          <AccordionBody steps={steps} />
+        </Accordion>
+      )}
+    </>
+  );
+}
+
+function AccordionBody({ steps }: { steps: WelcomeOnboardingData[] }) {
+  const router = useRouter();
+
+  return (
+    <>
+      {steps.map((step, index) => {
+        const Icon = step.icon;
+        return (
+          <Accordion.Item eventKey={`${index}`} key={index}>
+            <Accordion.Header>
+              <div className="mx-2">
+                {step.isDone ? (
+                  <img
+                    src="/images/check-filled.svg"
+                    alt="Completed or not"
+                    className={`p-0 border-0 ${
+                      step.isDone ? "onboarding done" : "onboarding not-done"
+                    }`}
+                    style={{ width: "1.2rem" }}
+                  />
+                ) : (
+                  <Icon size={25} stroke={1} className="navbar-icon" />
+                )}
+              </div>
+              <div className="mx-2">{step.title}</div>
+            </Accordion.Header>
+            <Accordion.Body>
+              <div className="my-2">{step.description}</div>
+              {step.buttonText && (
+                <div className="my-3">
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => router.push(step.onClickURL)}
+                  >
+                    {"+ " + step.buttonText}
+                  </button>
+                </div>
+              )}
+            </Accordion.Body>
+          </Accordion.Item>
+        );
+      })}
     </>
   );
 }
